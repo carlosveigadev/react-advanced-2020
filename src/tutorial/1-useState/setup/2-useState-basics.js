ca7b97c5ffc2cc6cfa6cf2463d0838ecfbdde06c
wrack <carlosveiga1618@gmail.com>
2021-02-24 22:52:51 -0300

    use useState hook to change text

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello world'))
+  // const value = useState(1)[0]
+  // const handler = useState(1)[1]
+  // console.log(value, handler)
+
+  const [text, setText] = useState('Random Title')
+
+  const handleClick = () => {
+    setText('hello world')
+  }
+
+  return (
+    <>
+      <h1>{text}</h1>
+      <button className='btn' onClick={handleClick}>Change Title</button>
+    </>
+    );
 };
 
 export default UseStateBasics;
